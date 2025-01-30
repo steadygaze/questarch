8fe3098be33f569822ad1276bd3764123616af6b
Steady Gaze <me@steadygaze.me>
2025-01-29 22:38:26 -0800

    Add missing use_app_state helper
    This got lost/accidentally reverted due to a git staging issue. Restore
    the code to a working state.

--- src/app_state.rs
+++ src/app_state.rs
@@ -7,3 +7,12 @@ pub struct AppState {
     pub db_pool: sqlx::postgres::PgPool,
     pub valkey_pool: fred::clients::Pool,
 }
+
+/// Wrapper to get AppState that's easily usable with the ? operator, for use in
+/// server functions.
+pub fn use_app_state() -> Result<AppState, ServerFnError> {
+    match use_context::<AppState>() {
+        Some(app_state) => Ok(app_state),
+        None => Err(ServerFnError::new("Couldn't get AppState from context")),
+    }
+}
