d63ae996e4fbd1abc248874893c74ef1dd607c0a
Steady Gaze <me@steadygaze.me>
2025-01-31 17:40:42 -0800

    Autofill and autohide in email form
    - Automatically show/hide the next stage of the login form based on the
      current state.
    - Replace email/challenge form inputs with autofilled hidden fields.

--- src/components/login.rs
+++ src/components/login.rs
@@ -1,5 +1,9 @@
+// For protyping only.
 #![allow(unused_variables)]
 #![allow(dead_code)]
+/// Login/new registration views.
+use crate::components::ui::*;
+use leptos::prelude::*;
 
 #[cfg(feature = "ssr")]
 use std::collections::HashMap;
@@ -11,7 +15,6 @@ use crate::key;
 
 #[cfg(feature = "ssr")]
 use fred::prelude::{HashesInterface, KeysInterface, TransactionInterface};
-use leptos::prelude::*;
 
 #[cfg(feature = "ssr")]
 use rand::{
@@ -19,8 +22,9 @@ use rand::{
     thread_rng,
 };
 
-const LOGIN_SESSION_EXPIRATION_SEC: i64 = 6 * 60 * 60;
+const LOGIN_SESSION_EXPIRATION_SEC: i64 = 20 * 60; // 20 minutes
 
+/// Email authentication first stage, where a challenge is generated/returned.
 #[server]
 pub async fn get_email_login_challenge(email: String) -> Result<String, ServerFnError> {
     let app_state = use_app_state()?;
@@ -41,26 +45,35 @@ pub async fn get_email_login_challenge(email: String) -> Result<String, ServerFn
     Ok(challenge)
 }
 
+/// Email authentication second stage, where the challenge is answered.
+///
+/// Note that, for security reasons, we can't tell the user which exactly of
+/// (email, challenge, response) was wrong.
 #[server]
 pub async fn answer_email_login_challenge(
     email: String,
     challenge: String,
     response: String,
 ) -> Result<bool, ServerFnError> {
-    let say_login_incorrect = || ServerFnError::new("Incorrect login information submitted");
-
     let app_state = use_app_state()?;
     let key = key::email_auth_code(&challenge);
-    let correct_data: HashMap<String, String> = app_state
+    let correct_data: HashMap<String, String> = match app_state
         .valkey_pool
         .hgetall::<Option<HashMap<String, String>>, _>(&key)
         .await?
-        .ok_or_else(say_login_incorrect)?;
+    {
+        Some(value) => value,
+        None => return Ok(false), // No matching challenge = wrong login.
+    };
 
-    let correct_email = correct_data.get("email").ok_or_else(say_login_incorrect)?;
-    let correct_response = correct_data
-        .get("response")
-        .ok_or_else(say_login_incorrect)?;
+    let correct_email = match correct_data.get("email") {
+        Some(value) => value,
+        None => return Ok(false), // Different email = wrong login.
+    };
+    let correct_response = match correct_data.get("response") {
+        Some(value) => value,
+        None => return Ok(false), // Wrong response = wrong login.
+    };
     if email == *correct_email && response == *correct_response {
         tokio::spawn(async move {
             if let Err(err) = app_state.valkey_pool.del::<(), _>(&key).await {
@@ -73,10 +86,32 @@ pub async fn answer_email_login_challenge(
     }
 }
 
+/// Main login page.
 #[component]
 pub fn Login() -> impl IntoView {
     let get_email_login_challenge = ServerAction::<GetEmailLoginChallenge>::new();
     let answer_email_login_challenge = ServerAction::<AnswerEmailLoginChallenge>::new();
+
+    let email = RwSignal::new("".to_string());
+    let challenge = RwSignal::new("".to_string());
+    // The last email address that was submitted (not the one currently entered
+    // into the form).
+    let last_email = RwSignal::new("".to_string());
+
+    let save_last_email = Effect::new(move || {
+        // input is cleared as soon as the server action resolves, so we must
+        // save this so the UI doesn't update then.
+        if let Some(server_last_email) = get_email_login_challenge.input().get() {
+            last_email.set(server_last_email.email);
+        }
+    });
+
+    let propogate_challenge = Effect::new(move || {
+        if let Some(Ok(server_challenge)) = get_email_login_challenge.value().get() {
+            challenge.set(server_challenge);
+        }
+    });
+
     // holds the latest *returned* value from the server
     let value = get_email_login_challenge.value();
     // check if the server has returned an error
@@ -85,51 +120,114 @@ pub fn Login() -> impl IntoView {
     view! {
         <h1 class="text-xl font-bold">"Login/register"</h1>
 
-        <ActionForm action=get_email_login_challenge>
-            <label for="email">Email challenge:</label>
-            <input
-                type="email"
-                name="email"
-                placeholder="email"
-                class="p-1 bg-gray-200 border border-gray-500 invalid:border-red-500"
-            />
-            <input
-                type="submit"
-                value="Email me"
-                class="py-0.5 px-2 bg-green-200 hover:bg-green-300"
-            />
-        </ActionForm>
+        <fieldset class="pt-1 pb-2 my-2 border-2 border-slate-500">
+            <legend class="font-bold text-l">Email challenge</legend>
 
-        <p>{move || if get_email_login_challenge.pending().get() { "Loading..." } else { "" }}</p>
-        <p>{move || format!("{:#?}", get_email_login_challenge.value().get())}</p>
+            <p>Receive and input a login code sent to the given email address.</p>
 
-        // TODO - Only show the form after the first stage.
-        <ActionForm action=answer_email_login_challenge>
-            <label for="email">Email challenge response:</label>
-            // TODO - Autofill as hidden fields.
-            <input
-                type="email"
-                name="email"
-                placeholder="email"
-                class="p-1 bg-gray-200 border border-gray-500 invalid:border-red-500"
-            />
-            <input
-                type="text"
-                name="challenge"
-                placeholder="challenge"
-                class="p-1 bg-gray-200 border border-gray-500 invalid:border-red-500"
-            />
-            <input
-                type="text"
-                name="response"
-                placeholder="response"
-                class="p-1 bg-gray-200 border border-gray-500 invalid:border-red-500"
-            />
-            <input
-                type="submit"
-                value="Submit challenge"
-                class="py-0.5 px-2 bg-green-200 hover:bg-green-300"
-            />
-        </ActionForm>
+            <ActionForm action=get_email_login_challenge>
+                <div class="flex gap-2">
+                    <label for="email">Email:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="email"
+                        class="px-1 h-full bg-gray-200 border border-gray-500 invalid:border-red-500"
+                        bind:value=email
+                    />
+                    <input
+                        type="submit"
+                        value="Email me"
+                        class="px-2 h-full bg-green-200 hover:bg-green-300"
+                    />
+                    <Show when=move || { *get_email_login_challenge.pending().read() }>
+                        <span class="self-center">
+                            <Spinner />
+                        </span>
+                    </Show>
+                </div>
+            </ActionForm>
+
+            <Show
+                when=move || {
+                    get_email_login_challenge.value().with(|val| matches!(val, Some(Ok(_))))
+                }
+                fallback=move || {
+                    view! {
+                        {move || {
+                            if let Some(Err(err)) = get_email_login_challenge.value().get() {
+                                format!("Error: {}", err.to_string())
+                            } else {
+                                format!("")
+                            }
+                        }}
+                    }
+                }
+            >
+                <p>
+                    An email has been sent to {last_email}
+                    with a login code; please enter it here within 20 minutes.
+                </p>
+
+                <ActionForm action=answer_email_login_challenge>
+                    <div class="flex gap-2">
+                        <input type="hidden" name="email" placeholder="email" bind:value=email />
+                        <input
+                            type="hidden"
+                            name="challenge"
+                            placeholder="challenge"
+                            bind:value=challenge
+                        />
+                        <label for="response">Login code:</label>
+                        <input
+                            type="text"
+                            name="response"
+                            placeholder="response"
+                            class="px-1 h-full bg-gray-200 border border-gray-500 invalid:border-red-500"
+                        />
+                        <input
+                            type="submit"
+                            value="Submit code"
+                            class="px-2 h-full bg-green-200 hover:bg-green-300"
+                        />
+                        <Show when=move || { answer_email_login_challenge.pending().get() }>
+                            <span class="self-center">
+                                <Spinner />
+                            </span>
+                        </Show>
+                    </div>
+                </ActionForm>
+
+                <Show
+                    when=move || {
+                        answer_email_login_challenge.value().with(|val| matches!(val, Some(Ok(_))))
+                    }
+                    fallback=move || {
+                        view! {
+                            {move || {
+                                if let Some(Err(err)) = answer_email_login_challenge.value().get() {
+                                    format!("Error: {}", err.to_string())
+                                } else {
+                                    format!("")
+                                }
+                            }}
+                        }
+                    }
+                >
+                    {move || {
+                        if let Some(Ok(accepted)) = answer_email_login_challenge.value().get() {
+                            if accepted {
+                                "Login code accepted."
+                            } else {
+                                "Login code rejected. Try again."
+                            }
+                        } else {
+                            ""
+                        }
+                    }}
+                </Show>
+            </Show>
+
+        </fieldset>
     }
 }
