80b50822ff75e8ab7d01e59b4f08665323998b07
Steady Gaze <me@steadygaze.me>
2025-02-09 21:30:52 -0800

    Make login menu helper more generic
    We may need to accept an image or something as the label soon (e.g. for
    logos of oauth providers).

--- src/components/auth/login.rs
+++ src/components/auth/login.rs
@@ -5,16 +5,16 @@ use leptos_router::components::A;
 #[component]
 fn LoginMethodEntry(
     endpoint: &'static str,
-    button_label: &'static str,
-    label: &'static str,
+    text_label: &'static str,
+    children: Children,
 ) -> impl IntoView {
     view! {
         <A href=endpoint>
             <div class="flex flex-col items-center">
                 <button class="p-4 text-4xl font-bold bg-transparent border-4 hover:bg-gray-300">
-                    {button_label}
+                    {children()}
                 </button>
-                <p>{label}</p>
+                <p>{text_label}</p>
             </div>
         </A>
     }
@@ -28,19 +28,23 @@ pub fn LoginMethods() -> impl IntoView {
             <fieldset class="px-2 pt-1 pb-2 border-2 border-slate-500">
                 <legend class="m-2 text-2xl font-bold">Login or register</legend>
                 <div class="flex flex-row gap-2">
-                    <LoginMethodEntry
-                        endpoint="email"
-                        button_label="📧 Email"
-                        label="Email login code"
-                    />
+                    <LoginMethodEntry endpoint="email" text_label="Email login code">
+                        "📧"
+                    </LoginMethodEntry>
                 </div>
             </fieldset>
             <fieldset class="px-2 pt-1 pb-2 border-2 border-slate-500">
                 <legend class="m-2 text-2xl font-bold">Login only</legend>
                 <div class="flex flex-row gap-2">
-                    <LoginMethodEntry endpoint="" button_label="?" label="?" />
-                    <LoginMethodEntry endpoint="" button_label="?" label="?" />
-                    <LoginMethodEntry endpoint="" button_label="?" label="?" />
+                    <LoginMethodEntry endpoint="" text_label="?">
+                        "?"
+                    </LoginMethodEntry>
+                    <LoginMethodEntry endpoint="" text_label="?">
+                        "?"
+                    </LoginMethodEntry>
+                    <LoginMethodEntry endpoint="" text_label="?">
+                        "?"
+                    </LoginMethodEntry>
                 </div>
             </fieldset>
         </div>
